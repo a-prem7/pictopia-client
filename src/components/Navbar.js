c587f7cc0ac9784102ddbb2e5e8eb84e1de442d6
amal-prem <aldamal.prem@gmail.com>
2023-05-18 16:48:27 -0400

    adds buttons to nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,35 @@
 import React from "react";
-import { AppBar, Box, Toolbar } from "@mui/material";
+import { AppBar, Box, Button, Toolbar } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <AppBar sx={{ p: "0 5%", background: "#2E3B55" }}>
-      <Toolbar>
+    <AppBar sx={{ p: "0 5%" }}>
+      <Toolbar sx={{ justifyContent: "space-between" }}>
         <h3> Pictopia </h3>
-        <Box></Box>
+        <Box sx={{ display: "flex", alignItems: "center", gap: 3 }}>
+          <Button>
+            <Link style={{ color: "#fff", textDecoration: "none" }} to={"/"}>
+              Home
+            </Link>
+          </Button>
+          <Button>
+            <Link
+              style={{ color: "#fff", textDecoration: "none" }}
+              to={"/login"}
+            >
+              Login
+            </Link>
+          </Button>
+          <Button>
+            <Link
+              style={{ color: "#fff", textDecoration: "none" }}
+              to={"/register"}
+            >
+              Register
+            </Link>
+          </Button>
+        </Box>
       </Toolbar>
     </AppBar>
   );
